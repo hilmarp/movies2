80e3da31355d7cd47f812b77fe3acec5d429a880
Hilmar <hilmar@hilmarp.com>
2014-06-26 23:02:28 +0000

    Myndi listi virkar

--- js/app.js
+++ js/app.js
@@ -1,3 +1,16 @@
-var movieApp = angular.module('movieApp', []);
+var movieApp = angular.module('movieApp', ['ngRoute']);
 
-movieApp.config()
+movieApp.config(function ($routeProvider) {
+	$routeProvider
+		.when('/', {
+			controller: 'ListController',
+			templateUrl: 'partials/list.html'
+		})
+		.when('/:movieId', {
+			controller: 'DetailsController',
+			templateUrl: 'partials/details.html'
+		})
+		.otherwise({
+			redirectTo: '/'
+		});
+});
--- js/controllers.js
+++ js/controllers.js
@@ -1,7 +1,12 @@
 movieApp.controller('ListController', function ($scope, $http) {
 
+	// Get list of movies
+	$http.get('http://apis.is/cinema').success(function (data) {
+		$scope.movies = data.results;
+	})
+
 });
 
-movieApp.controller('DetailsController', function ($scope) {
+movieApp.controller('DetailsController', function ($scope, $http) {
 
 });
